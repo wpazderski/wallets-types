b520beb89c1fa3e0cb53e7925f90fe165988403d
Witold Pazderski <witold.pazderski@gmail.com>
2022-12-26 18:24:27 +0100

    Code quality: fix exports order

--- src/data/index.d.ts
+++ src/data/index.d.ts
@@ -1,4 +1,4 @@
 export * as cryptocurrency from "./cryptocurrency";
 export * as currency from "./currency";
-export * as rates from "./rates";
 export * as market from "./market";
+export * as rates from "./rates";
